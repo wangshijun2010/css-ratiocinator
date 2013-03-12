afce17587d2e1ba775343388b67e9fd1ba883e16
Joe Nelson <cred+github@begriffs.com>
2013-03-11 17:13:56 -0700

    Output styles common to all media queries

--- ratiocinate.js
+++ ratiocinate.js
@@ -6,8 +6,11 @@
 
   var url, fonts,
     _               = require('./vendor/underscore-1.4.2.js'),
+    jQuery          = require('./vendor/jquery-1.8.2.js'),
     args            = require('system').args.slice(1),
     resource        = require('./lib/resource.js'),
+    obj             = require('./lib/obj.js'),
+    css             = require('./lib/css.js'),
     verbose         = false,
     isOptionOrFlag  = function (item) {
       return item.length > 0 && item[0] === '-';
@@ -36,6 +39,18 @@
         fonts = page.evaluate(function () { return CSS.fontDeclarations().join("\n\n"); }),
         toGo = intervals.length,
         combineIntervals = function () {
+          var properties = _.pluck(styles, 'properties'),
+            commonSelectors = _.uniq(_.flatten(_.map(properties, _.keys))),
+            commonStyle = _.reduce(
+              commonSelectors,
+              function (memo, selector) {
+                memo[selector] = obj.intersection(_.pluck(properties, selector));
+                return memo;
+              },
+              {}
+            );
+
+          console.log(_.map(commonStyle, css.renderStyle).join("\n"));
         },
         addStyle = function (interval) {
           return function (page) {
